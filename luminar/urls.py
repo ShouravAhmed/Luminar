7087f1d78dfda032f9e577f75fb5d8aacb0d1692
ah_med <ahmed15-898@diu.edu.bd>
2021-10-14 02:00:19 +0600

    environment fixed

--- luminar/urls.py
+++ luminar/urls.py
@@ -1,7 +1,7 @@
-"""Luminar URL Configuration
+"""luminar URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,14 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
